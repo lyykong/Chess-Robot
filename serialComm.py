07e65d0e7f34486cb3ca0b7dab1b240b82f8236f
lyykong <lc748@kent.ac.uk>
2025-02-11 12:29:38 +0000

    serial communication

--- serialComm.py
+++ serialComm.py
@@ -1,1 +1,12 @@
-print("hello world")
+import serial
+import time
+
+# Setting up arduino serial connection
+arduino = serial.Serial(port='COM8', baudrate=9600, timeout=1)
+time.sleep(2)
+
+arduino.write(b'Test Message\n')
+response = arduino.readline().decode('utf-8').strip()
+print("Received from Arduino:", response)
+
+arduino.close()
